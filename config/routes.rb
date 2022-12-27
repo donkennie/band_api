d57e99316b7dfa8dd741b370cacecaa5388f107e
donkennie <ajeigbekehinde160@gmail.com>
2022-12-27 03:09:15 +0100

    add Routing and Versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  resources :bands do
-    resources :members
+
+namespace :api do
+  namespace :v1 do
+    resources :bands do
+      resources :members
+    end
   end
+
+  namespace :v2 do
+    resources :members
+    resources :bands
+  end
+
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
